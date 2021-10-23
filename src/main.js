489fa4837cd52a878920010adc4a1313ac67c052
EremenkoVO <eremenko.v.o@yandex.ru>
2021-10-23 14:08:36 +0300

    update

--- src/main.js
+++ src/main.js
@@ -13,17 +13,7 @@ new Vue({
   router,
   render: (h) => h(App),
   created() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBPIDSnG3_d_qnRa_uih6m_NVtKOoJy6j8",
-      authDomain: "pages-history-lesson-mat-c6a0d.firebaseapp.com",
-      databaseURL:
-        "https://pages-history-lesson-mat-c6a0d-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "pages-history-lesson-mat-c6a0d",
-      storageBucket: "pages-history-lesson-mat-c6a0d.appspot.com",
-      messagingSenderId: "508167722264",
-      appId: "1:508167722264:web:13be25a2899b17ef403a6d",
-      measurementId: "G-QXZN5KTBCY",
-    };
+    const firebaseConfig = {};
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
