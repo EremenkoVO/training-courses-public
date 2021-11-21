93b16bda428efc017a5f24debfec659380125170
Vadim Eremenko <eremenko.v.o@yandex.ru>
2021-11-21 20:27:11 +0300

    fix app

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,19 @@
   <div id="nav"></div>
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface User {
+  login: string;
+  password: string;
+}
+
+export default defineComponent({
+  setup() {
+    var user: User;
+  },
+  data: () => ({}),
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount('#app')
+require('./firebase.config');
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app');
